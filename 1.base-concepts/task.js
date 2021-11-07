4ad5b82c0749b42e771d65d8d167aeb410784573
Irina <you@example.com>
2021-11-07 15:02:20 +0100

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,55 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  if (a == 0)
+   return "ошибка,  а=0 недопустимое значение";
+
+  let arr = [];
+    
+  let D = b*b - 4*a*c;
+  let x1, x2, x;
+
+  if (D > 0) {
+    x1 = (-b + Math.sqrt(D) )/(2*a);
+    x2 = (-b - Math.sqrt(D) )/(2*a);
+    
+    arr = [x1, x2];
+  }
+  else  if (D == 0)  {
+    x = -b/(2*a);
+    arr = [x];
+  }
+  return arr; 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
