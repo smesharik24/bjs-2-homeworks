d127549b4aa74dc32c9747199186452387d895e8
Irina <you@example.com>
2021-11-21 22:55:52 +0100

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,37 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result =
+  arr1.length === arr2.length &&
+  arr1.every(function (element, index) {
+    return element === arr2[index];
+  });  
 
   return result; // boolean
 }
 
+
+
+
+
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  // resultArr = arr.filter(function(num){
+  //   return num > 0;
+  // });
+
+  // resultArr = resultArr.filter(function(num){
+  //   return num % 3 === 0
+  // }); 
+  
+resultArr = arr.filter(function(num){
+  return (num >0 && num % 3 === 0);
+});
+
+  resultArr = resultArr.map(function(num){
+    return num*10;
+  });
 
   return resultArr; // array
 }
