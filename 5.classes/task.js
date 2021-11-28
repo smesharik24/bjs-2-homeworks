7de82f085b7d81bc2b405f1031bb55e1bbd3d5f4
smesharik24 <88437526+smesharik24@users.noreply.github.com>
2021-11-28 23:13:17 +0100

    Add files via upload
    nr1

---
+++ 5.classes/task.js
@@ -0,0 +1,29 @@
+
+//1
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    set state(state) {
+        if (state < 0) {
+            this._state = 0;
+        } else if (state > 100) {
+            this._state = 100;
+        } else {
+            this._state = state;
+        }
+    }
+}
