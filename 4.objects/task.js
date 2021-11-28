3431247247e00a842e2c8cf6e13aab29aafe66d4
smesharik24 <88437526+smesharik24@users.noreply.github.com>
2021-11-28 22:36:46 +0100

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,66 @@
-function Student(name, gender, age) {
-    // Ваш код
+// Задание 1
+function getArrayParams(arr) {
+  let min, max, sum, avg;
+    min = Infinity;
+    max = - Infinity;
+    sum = 0;
 
+  for ( let i=0; i<arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    } 
+    if (arr[i] > max){
+      max = arr[i];
+    } 
+    sum = sum + arr[i];
+  }
+    avg = sum / arr.length;
+    avg = Number(avg.toFixed(2));
+
+  return { min: min, max: max, avg: avg };
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+function makeWork(arrOfArr, func) {
+  let max = 0;
+
+  for (let i = 0; i<arrOfArr.length; i++){
+    a = func(arrOfArr[i]);
+    if (a>max ){
+      max = a;
+    }
+  }
+  
+  return max;
 }
 
-// ваш код для остальных методов
+// Задание 2
+function worker(arr) {
+  let sum;
+  sum = 0;
+  for (let i=0; i<arr.length; i++){
+    sum = sum + arr[i];
+  }
+
+  return sum;
+}
+
+
+// Задание 3
+function worker2(arr) {
+
+  let min, max;
+  min = Infinity;
+  max = - Infinity;
+
+  for (let i=0; i<arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    } 
+    if (arr[i] > max){
+      max = arr[i];
+    } 
+  }
+  
+  return max - min;
+}
+
