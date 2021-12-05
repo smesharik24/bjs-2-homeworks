9dfcd1e09a409197fc3944fa85dcf8a5f8c9d400
smesharik24 <88437526+smesharik24@users.noreply.github.com>
2021-12-05 17:42:50 +0100

    Add files via upload
    nr2

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -31,6 +31,7 @@ class PrintEditionItem {
 
 class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
+        super (name,releaseDate,pagesCount);
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
@@ -50,14 +51,14 @@ class Book extends PrintEditionItem {
 class NovelBook extends Book {
     constructor(autor, name, releaseDate, pagesCount) {
         super (name,releaseDate,pagesCount);
-       this.type = "novel";
+        this.type = "novel";
     }
 }
 
 class FantasticBook extends Book {
     constructor(autor, name, releaseDate, pagesCount) {
         super (name,releaseDate,pagesCount);
-       this.type = "fantastic";
+        this.type = "fantastic";
     }
 }
 
@@ -68,3 +69,24 @@ class DetectiveBook extends Book {
     }
 }
     
+
+
+
+//2
+
+
+
+findBookBy(type, value) {
+    if (this.books.find(book => book[type] === value)) {
+        return book;
+    }else if (this.books.find(book => book[type] == undefined)) {
+        return null;
+    }
+}
+
+// Ваша реализация должна быть такой:
+// Создать переменную, куда запишется результат метода (find). Значение переменной будет либо объект, либо undefined. Ничего другого метод find по определению вернуть не способен.
+// Исходя из того, что записалось в переменную из п.1 ретёрнуть либо объект, либо null
+
+// Не факт. Надо смотреть документацию к каждому методу. Но конкретно у find() колбэк всегда возвращает булево значение. Да и вообще, везде, где происходит проверка истинности в колбэке - там будет булево значение.
+// Но! Колбэк - это всего лишь функиця, которая передается в другую функцию в виде аргумента. Она может быть абсолютно любая. Вы можете в своей логике создавать и использовать функции-колбэки, которые будут возвращать то, что вам нужно, будь то булевы значения, строки, числа, объекты, массивы, что угодно. 
