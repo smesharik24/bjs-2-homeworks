5228063c8eae90e45d92c73bba262621da6cfcc1
smesharik24 <88437526+smesharik24@users.noreply.github.com>
2021-12-05 23:43:40 +0100

    Add files via upload
    nr1

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,114 @@
+
+//1
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    set state(state) {
+        if (state < 0) {
+            this._state = 0;
+        } else if (state > 100) {
+            this._state = 100;
+        } else {
+            this._state = state;
+        }
+    }
+}
+
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super (name,releaseDate,pagesCount);
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = "magazine"; 
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super (name,releaseDate,pagesCount);
+        this.author = author;
+        this.type = "book";
+    } 
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super (author,name,releaseDate,pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super (author,name,releaseDate,pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super (author,name,releaseDate,pagesCount);
+        this.type = "detective";
+    }
+}
+    
+
+
+
+//2
+
+class Library {
+    constructor(name, books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books = [book];
+        }
+    } 
+    
+    findBookBy(type, value) {
+
+        const found = this.books.find(book => book[type] === value);
+        if (found == undefined) 
+            return null;
+        
+        return found;
+    }
+
+    giveBookByName(bookName) {
+
+        const found = this.books.find(book => book.name === bookName);
+
+        if (found == undefined)
+            return null;
+
+        const index  = this.books.indexOf(found);
+
+        if (index > -1) {
+            this.books.splice(index, 1);
+        }
+
+        return found;
+
+    }
+}
